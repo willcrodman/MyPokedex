43c5a19e63b647e5bc18fc665136036aa72b7dca
wlliamrodman <willc.rodman@gmail.com>
2017-02-19 17:46:31 -0500

    Initial Commit

---
+++ My Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Pokedex
+//
+//  Created by William Rodman on 2/19/17.
+//  Copyright © 2017 William Rodman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
